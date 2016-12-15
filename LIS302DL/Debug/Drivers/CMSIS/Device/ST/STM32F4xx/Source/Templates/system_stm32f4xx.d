92f108332b4be5cf3efd8e333bf45bde8b12a2fe
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-12-15 21:26:46 +0700

    Use TIM3 Interrupt to trigger reading acceleration data and sending them via UART

---
+++ LIS302DL/Debug/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.d
@@ -0,0 +1,89 @@
+Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.o: \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Inc/stm32f4xx_hal_conf.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Inc/main.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Inc/stm32f4xx_hal_conf.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Inc/main.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+/Users/pirsquareff/Documents/Workspace/stm-project/LIS302DL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
